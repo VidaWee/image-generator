15d5c1c80f1131ba3dd4d334afeceb2489d97dae
Vida <134993237+VidaWee@users.noreply.github.com>
2023-07-04 18:32:10 +1200

    Fetch random image array

--- js/script.js
+++ js/script.js
@@ -2,3 +2,36 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = async function(){
+
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+    const images = await res.json();
+    //console.log(images);
+    selecRandomImage(images);
+};
+
+const selecRandomImage = function(images){
+
+    const randomIndex = Math.floor(Math.random * images * length);
+    //console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    //console.log(randomImage);
+    displayImage(randomImage);
+};
+
+const displayImage = function(randomImage){
+
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+
+};
+button.addEventListener("click", function() {
+
+    getImage();
+
+});
+
